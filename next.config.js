3d86904f2c523664ae378abffe3fde647d91cf75
Emmanuel Chierchie <emmchierchie@gmail.com>
2022-07-19 22:29:53 -0300

    deleting experimiental

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputStandalone: true,
-  },
   env: {
     REACT_APP_CONTENTFUL_SPACE_ID: '5gkfl4mk2m59',
     REACT_APP_ACCESS_TOKEN: 'p1CQVS_MXM1WczNEJYonf-DMr32DbZCIuMtEHk_vY-E',
