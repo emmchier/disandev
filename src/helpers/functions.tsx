29f44219e68139e5ed0c35cbe8b2bde1eea05dba
Emmanuel Chierchie <emmchierchie@gmail.com>
2022-07-19 22:27:22 -0300

    traying fix errors

--- src/helpers/functions.tsx
+++ src/helpers/functions.tsx
@@ -1,11 +1,11 @@
 import { KeywordInterface, PageInterface, SocialMedia } from '../interfaces/index';
 
 export const getPage = (list: PageInterface[], slug: string) =>
-  list?.filter((page) => page.slug === slug) || [];
+  list?.filter((page) => page?.slug === slug) || [];
 
 export const getKeywords = (keywords: KeywordInterface[]) => {
-  const getKeywords = keywords?.map((keyword) => keyword.fields.word);
-  return getKeywords?.join(',') || '';
+  const getKeywords = keywords?.map((keyword) => keyword?.fields?.word);
+  return getKeywords.join(',') || '';
 };
 
-export const getSocialNetworks = (list: SocialMedia[]) => list?.map((item) => item.fields) || [];
+export const getSocialNetworks = (list: SocialMedia[]) => list?.map((item) => item?.fields) || [];
--- src/hooks/usePageMetadata.tsx
+++ src/hooks/usePageMetadata.tsx
@@ -2,11 +2,11 @@ import { PageInterface } from '../interfaces';
 import { getKeywords, getPage } from '../helpers/functions';
 
 export const usePageMetadata = (pages: PageInterface[], slug: string) => {
-  const formatedList = pages.map((page) => page.fields);
+  const formatedList = pages?.map((page) => page?.fields);
   const page = getPage(formatedList, slug);
-  const title = page[0].title;
-  const description = page[0].description;
-  const keywords = getKeywords(page[0].keywords);
+  const title = page[0]?.title;
+  const description = page[0]?.description;
+  const keywords = getKeywords(page[0]?.keywords);
 
   return { page, title, description, keywords };
 };
