ebfe0da85fb5f61e1f343b8ba539de6389ee4175
Emmanuel Chierchie <emmchierchie@gmail.com>
2022-07-21 00:44:37 -0300

    fixing text

--- src/components/atomic-design/atoms/text/index.tsx
+++ src/components/atomic-design/atoms/text/index.tsx
@@ -4,20 +4,13 @@ import { Paragraph } from './styles';
 
 type TextI = {
   children: React.ReactNode;
-  variant?: string;
   weight?: string;
   color?: string;
   cap?: string;
 };
 
-const Text = ({
-  children,
-  variant = 'body1',
-  weight = 'regular',
-  color = 'dark',
-  cap = 'default',
-}: TextI) => (
-  <Paragraph variant={variant} weight={weight} color={color} cap={cap}>
+const Text = ({ children, weight = 'regular', color = 'dark', cap = 'default' }: TextI) => (
+  <Paragraph weight={weight} color={color} cap={cap}>
     {children}
   </Paragraph>
 );
