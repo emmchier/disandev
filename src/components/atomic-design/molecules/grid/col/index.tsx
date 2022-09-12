153db7163b2a177881c61830d733cfcae0a2af84
Emmanuel Chierchie <emmchierchie@gmail.com>
2022-09-12 09:08:54 -0300

    traying fix grid on big screens

--- src/components/atomic-design/molecules/grid/col/index.tsx
+++ src/components/atomic-design/molecules/grid/col/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 import { ColContent, GridCol, GridItemProps } from './styles';
 
-const Col: FC<GridItemProps> = ({ children, xs = 12, sm = 12, md = 12, lg = 12, xlg = 12 }) => (
+const Col: FC<GridItemProps> = ({ children, xs, sm, md, lg, xlg }) => (
   <GridCol xs={xs} sm={sm} md={md} lg={lg} xlg={xlg}>
     <ColContent>{children}</ColContent>
   </GridCol>
--- src/components/atomic-design/molecules/grid/col/styles.tsx
+++ src/components/atomic-design/molecules/grid/col/styles.tsx
@@ -46,10 +46,11 @@ export const GridCol = styled.li<GridItemProps>`
   display: flex;
   list-style: none;
   flex-grow: 0;
+  flex-basis: ${({ xlg }) => getBreackpoint(xlg)};
 
-  @media only screen and (${({ theme }) => theme.breakpoints.desktop}) {
+  /* @media only screen and (${({ theme }) => theme.breakpoints.desktop}) {
     flex-basis: ${({ xlg }) => getBreackpoint(xlg)};
-  }
+  } */
   @media only screen and (${({ theme }) => theme.breakpoints.tabletLandscape}) {
     flex-basis: ${({ lg }) => getBreackpoint(lg)};
   }
